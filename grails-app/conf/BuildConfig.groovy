794df6f55ac3d04260719ad8aa38f75229cb16d3
Jeff Scott Brown <jbrown@gopivotal.com>
2014-02-14 13:20:41 -0600

    Upgrade to Grails 2.3.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,20 +1,33 @@
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
-        // uncomment to disable ehcache
+        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
         // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
     repositories {
+        inherits true // Whether to inherit repository definitions from plugins
+
         grailsPlugins()
         grailsHome()
         grailsCentral()
-        
+
         mavenRepo 'http://dist.gemstone.com/maven/release'
+
+        mavenRepo "http://repo.grails.org/grails/core"
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
@@ -28,26 +41,37 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         // runtime 'mysql:mysql-connector-java:5.1.5'
+        compile 'org.springframework:spring-jms:3.2.5.RELEASE'
 
         test 'org.codehaus.geb:geb-spock:0.7.0'
         test("org.seleniumhq.selenium:selenium-htmlunit-driver:2.21.0") {
             exclude "xml-apis"
         }
     }
-    
+
     plugins {
-        build ":tomcat:$grailsVersion"
-        
+        // plugins for the build system only
+        build ":tomcat:7.0.50"
+
+        // plugins for the compile step
+        compile ":scaffolding:2.0.1"
+        compile ':cache:1.1.1'
+
+        // plugins needed at runtime but not for compilation
+        runtime ":hibernate:3.6.10.7" // or ":hibernate4:4.1.11.6"
+        runtime ":database-migration:1.3.8"
+        runtime ":jquery:1.10.2.2"
+        // Uncomment these (or add new ones) to enable additional resources capabilities
+        //runtime ":zipped-resources:1.0.1"
+        //runtime ":cached-resources:1.1"
+        //runtime ":yui-minify-resources:0.1.5"
+
         compile ':activemq:0.1'
-        compile ":hibernate:$grailsVersion"
-        compile ':jms:1.0'
-        compile ':jquery:1.6.1.1'
-        compile ':searchable:0.6.3'
+        compile ':jms:1.2'
+        compile ':searchable:0.6.6'
         compile ':spring-security-core:1.2.4'
         test ':geb:0.7.0'
         test ':spock:0.6'
-        compile ":cache:1.0.0.RC1"
         test ':functional-test:1.2.7'
     }
-    
 }
--- grails-app/views/index.gsp
+++ grails-app/views/index.gsp
@@ -59,7 +59,7 @@
                     <ul>
                         <li>App version: <g:meta name="app.version"></g:meta></li>
                         <li>Grails version: <g:meta name="app.grails.version"></g:meta></li>
-                        <li>Groovy version: ${org.codehaus.groovy.runtime.InvokerHelper.getVersion()}</li>
+                        <li>Groovy version: ${GroovySystem.getVersion()}</li>
                         <li>JVM version: ${System.getProperty('java.version')}</li>
                         <li>Controllers: ${grailsApplication.controllerClasses.size()}</li>
                         <li>Domains: ${grailsApplication.domainClasses.size()}</li>
