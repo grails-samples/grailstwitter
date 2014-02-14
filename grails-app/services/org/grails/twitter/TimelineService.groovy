5f5a2f48df4df0c5dcba083aa03b2523bc440090
Jeff Scott Brown <jbrown@gopivotal.com>
2014-02-14 13:21:39 -0600

    remove caching for now
    Need to look into why the caching isn't working in 2.3.5.  It looks like cache eviction may not be behaving correctly.

--- grails-app/services/org/grails/twitter/TimelineService.groovy
+++ grails-app/services/org/grails/twitter/TimelineService.groovy
@@ -2,17 +2,12 @@ package org.grails.twitter
 
 import org.grails.twitter.auth.Person
 
-import grails.plugin.cache.CacheEvict
-import grails.plugin.cache.Cacheable
-
 class TimelineService {
 
 	static transactional = false
-	
-    @CacheEvict('timeline')
+
     void clearTimelineCacheForUser(String username) {}
-    
-    @Cacheable('timeline')
+
     def getTimelineForUser(String username) {
         def per = Person.findByUsername(username)
         def query = Status.whereAny {
